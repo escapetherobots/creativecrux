35413e7c7d5fb552051b08f900a5f0de41a10fa2
r2z2 <escapetherobots@gmail.com>
2017-01-10 14:49:23 -0700

    Category Model and Assigned projects to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110205238) do
+ActiveRecord::Schema.define(version: 20170110214500) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +25,8 @@ ActiveRecord::Schema.define(version: 20170110205238) do
     t.string   "designer"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.integer  "category_id"
   end
 
   create_table "users", force: :cascade do |t|
