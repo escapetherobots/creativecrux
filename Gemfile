2959a9eb0066cf88563e6f955fe46b431db783b2
r2z2 <escapetherobots@gmail.com>
2017-01-10 12:15:47 -0700

    Add project form and controller actions(show index new and create)

--- Gemfile
+++ Gemfile
@@ -23,8 +23,10 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'hirb'
 gem 'simple_form', '~> 3.4'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
+gem 'susy'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
@@ -40,6 +42,7 @@ group :development, :test do
   gem 'byebug'
 
   gem 'sqlite3'
+
 end
 
 group :development do
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,35 @@
 class ProjectsController < ApplicationController
+	before_action :find_project, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@projects = Project.all.order("created_at DESC")
+	end
+
+	def show
+		#runs the before_action
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new(project_params)
+
+		if @project.save
+			redirect_to root_path #root path = projects
+		else
+			render 'new' #render does not refresh the page
+		end
+	end
+
+	private
+
+	def project_params
+		params.require(:project).permit(:title, :description, :designer)
+	end
+
+	def find_project
+		@project = Project.find(params[:id])
 	end
 end
