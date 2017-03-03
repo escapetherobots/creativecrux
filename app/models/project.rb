89b38a090b88bf10ed9b34748668bf76aa26c418
r2z2 <escapetherobots@gmail.com>
2017-03-03 10:00:32 -0700

    Update styles for projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,13 @@ class Project < ActiveRecord::Base
 	accepts_nested_attributes_for :project_image, reject_if: :all_blank, allow_destroy: true
 
 	validates :title, :description, presence: true
+
+
+	def to_param
+    "#{self.id}-#{self.title}"
+	end
+
+	def getMyId
+		self.title
+	end
 end
