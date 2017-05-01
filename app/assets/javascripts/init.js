8cc9e8ea6e27148749aa1ffd2f722f1ce4869aed
r2z2 <escapetherobots@gmail.com>
2017-05-01 12:06:30 -0600

    Update Show page styles, homepage styles and, main banner styles

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -5,21 +5,99 @@ CruxApp.init = function(){
 	//this.fitTexter();
 	this.sceneBuilder();
 	this.sideNav();
-	this.snap();
-	this.snap2();
-	this.snap3();
+	//this.trig();
 	//this.particleBg();
+
+	if ( $('.z-uxContainer').length ) {
+		this.snap();
+		this.snap2();
+		this.snap3();
+	} else {
+		console.log('no snap');
+	}
+		
 }
+
+CruxApp.trig = function(){
+	function fun1(x) {return Math.sin(x);}
+	function fun2(x) {return Math.cos(6*x);}
+
+	function draw() {
+		var canvas = document.getElementById('myCanvas');
+		if (null == canvas || !canvas.getContext) return;
+
+		var axes = {},
+			ctx = canvas.getContext('2d');
+
+		axes.x0 = .5 + .5*canvas.width;
+
+		axes.y0 = .5 + .5*canvas.height;
+		axes.y1 = axes.y0 + .5;
+		axes.scale = 100;
+		axes.doNegativeX = true;
+
+		showAxes(ctx, axes);
+		funGraph(ctx, axes, fun1, "rgb(255,255,255)", 5);
+		funGraph(ctx, axes, fun2, "rgb(0,0,0)", 2);
+	}
+
+	function funGraph (ctx,axes,func,color,thick) {
+		 var xx, yy, dx=2, x0=axes.x0, y0=axes.y0, scale=axes.scale;
+		 var iMax = Math.round((ctx.canvas.width-x0)/dx);
+		 var iMin = axes.doNegativeX ? Math.round(-x0/dx) : 0;
+		 ctx.beginPath();
+		 ctx.lineWidth = thick;
+		 ctx.strokeStyle = color;
+
+		 for (var i=iMin;i<=iMax;i++) {
+		  xx = dx*i; yy = scale*func(xx/scale);
+		  if (i==iMin) ctx.moveTo(x0+xx,y0-yy);
+		  else         ctx.lineTo(x0+xx,y0-yy);
+		 }
+		 ctx.stroke();
+	}
+
+		function showAxes(ctx,axes) {
+		 var x0=axes.x0, canvasWidth=ctx.canvas.width;
+		 var x1 = axes.x1;
+		 var y0=axes.y0, h=ctx.canvas.height;
+		 var xmin = axes.doNegativeX ? 0 : x0;
+		 ctx.beginPath();
+		 ctx.strokeStyle = "rgb(128,128,128)"; 
+		 ctx.moveTo(xmin,y0 +1); ctx.lineTo(canvasWidth,y0+1);  // X axis
+		 ctx.moveTo(x0, 0);    ctx.lineTo(x0,h);  // Y axis
+		 ctx.stroke();
+		 ctx.closePath();
+
+		 ctx.beginPath();
+		ctx.moveTo(canvasWidth / 2.5, h/2);
+		ctx.lineTo(canvasWidth / 2.5, h);
+		ctx.stroke();
+		ctx.closePath();
+		}
+
+		draw();
+
+
+} // end trig
+
 
 //toggle effect
 CruxApp.toggler = function(){
 	$(document)
 		.off('click', '[data-test]')
 		.on('click', '[data-test]', function(){
-  		console.log($(this).next());
-  	
-    	$(this).next().fadeToggle();
-    });
+			var that = $(this);
+			if (that.hasClass('show-toggle')) {
+				that.removeClass('show-toggle');
+				that.text('SHOW DETAILS');
+				that.next().slideToggle(500);
+			} else {
+				that.addClass('show-toggle'); 
+				that.text('HIDE DETAILS');
+				that.next().slideToggle(500);
+			}
+    	});
 }
 
 
